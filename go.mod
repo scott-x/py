b0a4e7f23d22f096449f771f97ff856f1843d89c
scottxiong <github.scott.bear@gmail.com>
2020-05-26 15:29:51 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/scott-x/py
+
+go 1.12
+
+require github.com/scott-x/gutils v0.0.0-20200515213001-6a614cf8e3df
--- main.go
+++ main.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"github.com/scott-x/gutils/fs"
-	"github.com/scott-x/gutils/cmd"
 	"github.com/scott-x/gutils/cl"
-	// "github.com/scott-x/gutils/cmd"
+	"github.com/scott-x/gutils/cmd"
+	"github.com/scott-x/gutils/fs"
 )
 
-var TEMPLATE_FOLDER = fs.GetEnv("GOPATH")+"/src/github.com/scott-x/py/pkg"
+var TEMPLATE_FOLDER = fs.GetEnv("GOPATH") + "/src/github.com/scott-x/py/pkg"
 
 func main() {
 	cmd.AddQuestion("name", "What's your pip installed name: ", "Please input correct name: ", "^[a-z]+")
 	answers := cmd.Exec()
-    name := answers["name"]
-    fs.CreateDirIfNotExist("./scott")
-    err :=fs.CopyFolder(TEMPLATE_FOLDER,"./scott")
-    if err!=nil{
-    	panic(err)
-    }
-    fs.Rename("./scott/slimz","./scott/"+name)
-    err = fs.ReadAndReplace("./scott/setup.py",map[string]string{
-    	"slimz": name,
-    })
-    if err!=nil{
-    	panic(err)
-    }
-    cmd.Info( name +" was created successfully, please make sure setuptools、 wheel、 twine are installed. If not, please install with following command:")
-    cl.BoldCyan.Println("$ python3 -m pip install --user --upgrade setuptools wheel twine")
+	name := answers["name"]
+	fs.CreateDirIfNotExist("./scott")
+	err := fs.CopyFolder(TEMPLATE_FOLDER, "./scott")
+	if err != nil {
+		panic(err)
+	}
+	fs.Rename("./scott/slimz", "./scott/"+name)
+	err = fs.ReadAndReplace("./scott/setup.py", map[string]string{
+		"slimz": name,
+	})
+	if err != nil {
+		panic(err)
+	}
+	cl.BoldRed.Printf(name)
+	cl.BoldGreen.Printf(" was created successfully ^_^\n")
+	cl.BoldYellow.Printf("Please make sure setuptools、wheel、twine are installed as well. If not, please install with following command:\n")
+	cl.BoldCyan.Println("$ python3 -m pip install --user --upgrade setuptools wheel twine")
 }
